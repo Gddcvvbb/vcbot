fa59ae1c9bfd31c7545deaae0071bf42e732bd5c
Gddcvvbb <119120655+Gddcvvbb@users.noreply.github.com>
2022-12-03 16:26:05 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     API_ID = int(os.environ.get("APP_ID", "21247842"))
-    API_HASH = os.environ.get("API_HASH", ")
+    API_HASH = os.environ.get("API_HASH", "9cdb0caff1fae351aa3bcaf997560b7b")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", ")
     STRING_SESSION = os.environ.get("STRING_SESSION", "
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
