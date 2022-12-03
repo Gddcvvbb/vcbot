8a63c29d671cfa42a77cb39c5786998badd0f227
Gddcvvbb <119120655+Gddcvvbb@users.noreply.github.com>
2022-12-03 15:19:34 +0530

    Update Config.py
    1AZWarzUBu1H2D0Sbv8GMjG12Le5mV8KjNWKaTz8laHENvzfsMGYtMD93E_qhKsJznXbQw4UewauDVrLiIQ5Yq7hSjHbqV3AhgcGbt8okc-2wRqcBoxpRzlBKPhmSTBKQAiyIkO-EzZ2g8D3_WYBRZUlI1r2f4NomVh2Qks0vm0W1heD2sJtRMiBla2vC9-oVNSS5QYUQmXYFhZeLIdO0dduqm4ZL9lWY954jG5S_1b9EfeICLHEc0e8OiELUU_MXZdszV_NStX--ZaesMehKlhWx1qe7bl7cr2_iYxaL6ixumWAfmsEKsB4aw-zlKgJUYj9qdfuj5V5xY_ZiXFnxKcb7fmyndeg=")
        MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)

--- Config.py
+++ Config.py
@@ -2,12 +2,11 @@ import os
 
 class Config(object):
     API_ID = int(os.environ.get("APP_ID", "21247842"))
-    API_HASH = os.environ.get("API_HASH", "9cdb0caff1fae351aa3bcaf997560b7b")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5846369400:AAELi6U-AN92Y-Os7LtlJkdZEI_IFb5aL8I")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "1AZWarzUBu1H2D0Sbv8GMjG12Le5mV8KjNWKaTz8laHENvzfsMGYtMD93E_qhKsJznXbQw4UewauDVrLiIQ5Yq7hSjHbqV3AhgcGbt8okc-2wRqcBoxpRzlBKPhmSTBKQAiyIkO-EzZ2g8D3_WYBRZUlI1r2f4NomVh2Qks0vm0W1heD2sJtRMiBla2vC9-oVNSS5QYUQmXYFhZeLIdO0dduqm4ZL9lWY954jG5S_1b9EfeICLHEc0e8OiELUU_MXZdszV_NStX--ZaesMehKlhWx1qe7bl7cr2_iYxaL6ixumWAfmsEKsB4aw-zlKgJUYj9qdfuj5V5xY_ZiXFnxKcb7fmyndeg=")
-    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
+    API_HASH = os.environ.get("API_HASH", ")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", ")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Vcplayertes_bot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", ")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
